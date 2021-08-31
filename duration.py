62afe784a0a90efa590a0f8f28f18fe2ddde76c2
dipudas8984 <dasdipu8984@gmail.com>
2021-08-31 16:10:10 +0530

    minor changees

--- duration.py
+++ duration.py
@@ -1,4 +1,7 @@
 import os, sys
+import itertools
+import threading
+import time
 
 
 def get_length(list):
@@ -21,6 +24,22 @@ def convert(seconds):
     return hours, mins, seconds
 
 
+done = False
+#here is the animation
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rloading ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\r')
+
+t = threading.Thread(target=animate)
+
+
+#long process here
+
 argv_list = sys.argv
 
 if __name__ == '__main__':
@@ -32,10 +51,9 @@ if __name__ == '__main__':
         if len(list) == 0:
             print('No Suppported Video Files here')
             exit()
-    
+    t.start()
     from moviepy.editor import VideoFileClip
-
-
     hour , mins, secs = convert(get_length(list))
+    done = True
     print(f'Total Duration: {hour}:{mins}:{secs}')
 
